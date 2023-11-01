9adb6de1bfa8565de11d666f996400e8d39059d0
nathantran00 <nathantrxn@gmail.com>
2023-10-31 20:19:18 -0700

    preloader image for 3d obj
    Implemented a stand-in image until the 3d model-viewer object is done loading

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,22 +4,22 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'figtree': ['Figtree', 'serif'],
-        'labelle': ['La Belle Aurore', 'cursive'],
+        figtree: ["Figtree", "serif"],
+        labelle: ["La Belle Aurore", "cursive"],
       },
       colors: {
-        'dark-gray': '#232324',
-        'black': '#000000',
-        'white': '#fafafa',
-        'pinkish-red': '#da627d',
-        'light-gray': '#f2f2f3',
-        'off-white': '#f4f1de',
-        'maroon': '#42091F',
-        'gray': '#808080',
-        'medium-gray': '#555555',
-        'darker-gray': '#404040',
+        "dark-gray": "#232324",
+        black: "#000000",
+        white: "#fafafa",
+        "pinkish-red": "#da627d",
+        "light-gray": "#f2f2f3",
+        "off-white": "#f4f1de",
+        maroon: "#42091F",
+        gray: "#808080",
+        "medium-gray": "#555555",
+        "darker-gray": "#404040",
       },
     },
   },
   plugins: [],
-}
+};
